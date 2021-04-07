f4a1a8e6a88851020d400b1cac8adc7320a64681
engin can <engin_can1528@hotmail.com>
2021-04-07 22:52:55 +0300

    Design Pattern

---
+++ KodOrnek/Behavioral/ChainOfResponsibility/JPEGHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.ChainOfResponsibility
+{
+    // JPG dosyalarını dönüştüren sınıf.
+    // UML diyagramındaki ConcreteHandler sınıfına denk gelmektedir.
+    public class JPEGHandler : BaseHandler
+    {
+        public override void HandleRequest(Image image)
+        {
+            if (image.Extension == "JPEG")
+            {
+                Console.WriteLine("JPEG to PNG");
+                //JPEG Dönüştürme işlemleri
+            }
+            else
+            {
+                //Bu sınıfa ait bir işlem değilse zincirin bir sonraki halkasına aktarılır
+                _nextHandler.HandleRequest(image);
+            }
+        }
+    }
+}
